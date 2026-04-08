324e87572c84bbffe6ffd7650f8519446d41e383
sarvesh-astrophile <sarvesh9301@gmail.com>
2026-04-08 21:07:24 +0530

    Github oauth

--- apps/web/src/routes/auth.tsx
+++ apps/web/src/routes/auth.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { AuthScreen } from "@/features/auth/components/auth-screen";
 
 export const Route = createFileRoute("/auth")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/signin"!</div>;
+	return <AuthScreen />;
 }
