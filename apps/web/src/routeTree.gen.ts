518b5370dbb5ce8d8a4bfe4bca8a1d5a8f6907e8
sarvesh-astrophile <sarvesh9301@gmail.com>
2026-04-08 20:52:22 +0530

    Update routeTree.gen.ts

--- apps/web/src/routeTree.gen.ts
+++ apps/web/src/routeTree.gen.ts
@@ -9,7 +9,7 @@
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
 import { Route as rootRouteImport } from './routes/__root'
-import { Route as SigninRouteImport } from./routes/_protected/test.tsxgnin'
+import { Route as SigninRouteImport } from './routes/signin'
 import { Route as ProtectedRouteImport } from './routes/_protected'
 import { Route as IndexRouteImport } from './routes/index'
 import { Route as ProtectedTestRouteImport } from './routes/_protected/test'
