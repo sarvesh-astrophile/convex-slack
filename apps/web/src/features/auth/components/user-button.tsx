486c565585c687e712ba1214d9df30f4d88f8c17
sarvesh-astrophile <sarvesh9301@gmail.com>
2026-04-10 07:40:56 +0530

    avatart

---
+++ apps/web/src/features/auth/components/user-button.tsx
@@ -0,0 +1,45 @@
+import { useAuthActions } from "@convex-dev/auth/react";
+import {
+	Avatar,
+	AvatarFallback,
+	AvatarImage,
+} from "@open-slack/ui/components//avatar";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@open-slack/ui/components/dropdown-menu";
+import { LoaderCircle, LogOut } from "lucide-react";
+import { useCurrentUser } from "../hooks/user-current-user";
+
+export const UserButton = () => {
+	const { signOut } = useAuthActions();
+	const { data, isLoading } = useCurrentUser();
+	if (isLoading)
+		return (
+			<LoaderCircle className="size-4 animate-spin text-muted-foreground" />
+		);
+	if (!data) return null;
+	const { image, name } = data;
+	const avatarFallback = name ? name[0] : null;
+
+	return (
+		<DropdownMenu modal={false}>
+			<DropdownMenuTrigger>
+				<Avatar size="lg" className="relative outline-none">
+					<AvatarImage src={image} alt={name} />
+					<AvatarFallback>{avatarFallback}</AvatarFallback>
+				</Avatar>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent side="right" align="center" className="rounded-md">
+				<DropdownMenuGroup>
+					<DropdownMenuItem onClick={signOut}>
+						<LogOut /> Logout
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
--- apps/web/src/routes/_protected/test.tsx
+++ apps/web/src/routes/_protected/test.tsx
@@ -1,23 +1,15 @@
-import { useAuthActions } from "@convex-dev/auth/react";
-import { Button } from "@open-slack/ui/components/button";
 import { createFileRoute } from "@tanstack/react-router";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export const Route = createFileRoute("/_protected/test")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	const { signOut } = useAuthActions();
 	return (
 		<div>
 			Hello "/test"!
-			<Button
-				onClick={() => {
-					signOut();
-				}}
-			>
-				Sign Out
-			</Button>
+			<UserButton />
 		</div>
 	);
 }
